1c75cfb0d67a154e19be222e4bcda95bc0dc2803
haneul <haneul@coinone.com>
2025-09-11 18:04:32 +0900

    strudel lets goooo

---
+++ nvim/.config/nvim/lua/runjivu/lazy/strudel.lua
@@ -0,0 +1,22 @@
+return {
+  "gruvw/strudel.nvim",
+  build = "npm install",
+  config = function()
+    local strudel = require("strudel")
+    strudel.setup()
+    -- Strudel keymaps
+    vim.keymap.set("n", "<leader>sl", strudel.launch, { desc = "Launch Strudel" })
+    vim.keymap.set("n", "<leader>sq", strudel.quit, { desc = "Quit Strudel" })
+    vim.keymap.set("n", "<leader>st", strudel.toggle, { desc = "Strudel Toggle Play/Stop" })
+    vim.keymap.set("n", "<leader>su", strudel.update, { desc = "Strudel Update" })
+    vim.keymap.set("n", "<leader>sb", strudel.set_buffer, { desc = "Strudel set current buffer" })
+    vim.keymap.set("n", "<leader>sx", strudel.execute, { desc = "Strudel set current buffer and update" })
+    -- Auto-update on save for JavaScript/Strudel files
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = "*.str",
+      callback = function()
+        strudel.update()
+      end,
+    })
+  end,
+}
