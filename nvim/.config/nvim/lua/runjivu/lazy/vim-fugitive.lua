c9c1083ab2f9aa67e62694a3d411973a17a42f6b
haneul <haneul@coinone.com>
2026-02-24 15:51:10 +0900

    added vim-fugitive auto git pull config

--- nvim/.config/nvim/lua/runjivu/lazy/vim-fugitive.lua
+++ nvim/.config/nvim/lua/runjivu/lazy/vim-fugitive.lua
@@ -4,5 +4,13 @@ return {
         vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
         vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
         vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+        
+        -- Override P key in fugitive summary buffer to use pull --rebase && push
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = 'fugitive',
+            callback = function()
+                vim.keymap.set('n', 'P', ':Git pushup<CR>', { buffer = true })
+            end,
+        })
     end
 }
